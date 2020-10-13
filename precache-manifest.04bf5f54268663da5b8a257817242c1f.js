9e72f6884ff482f04b047c3385bc9785c643370f
Aleksandr0012 <32939018+Aleksandr0012@users.noreply.github.com>
2020-10-14 00:44:15 +0400

    Updates

---
+++ precache-manifest.04bf5f54268663da5b8a257817242c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ae983d91190c362531bf9607f2cc85",
+    "url": "/taxi/index.html"
+  },
+  {
+    "revision": "fc2d7519b0234ea6afac",
+    "url": "/taxi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb7982c69d159a012ff6",
+    "url": "/taxi/static/js/2.4bd92ce0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taxi/static/js/2.4bd92ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2d7519b0234ea6afac",
+    "url": "/taxi/static/js/main.5739db09.chunk.js"
+  },
+  {
+    "revision": "8dd6db9035e73a7e9242",
+    "url": "/taxi/static/js/runtime-main.f686bdfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taxi/static/media/logo.5d5d9eef.svg"
+  }
+]);
